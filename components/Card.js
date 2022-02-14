37575a396ad65e52886b0c822d500badbed8c53c
Lucas <lucasalonso11800@gmail.com>
2022-02-13 22:43:04 -0300

    Added pro and rank flairs

--- components/Card.js
+++ components/Card.js
@@ -4,18 +4,21 @@ import React from 'react';
 import classes from '../styles/Card.module.css';
 
 export default function Card({ car }) {
-    const { title, drivername, users, bookmarks, car_make_name, dpic, pic, slink, dlink } = car;
+    const { title, drivername, users, bookmarks, car_make_name, dpic, pic, slink, dlink, pro_user, rank_txt } = car;
+
     return (
         <div className={classes.cardContainer}>
             <div className={classes.card}>
                 <div className={classes.cardHeader}>
                     <Link href={dlink}><Image src={dpic} width={32} height={32} /></Link>
                     <Link href={dlink}>{drivername}</Link>
+                    {pro_user && <span className={classes.pro}>PRO</span>}
                 </div>
                 <div className={classes.cardBody}>
                     <Link href={slink}>
                         <Image src={pic} height={206} width={300} layout="responsive" />
                     </Link>
+                    {rank_txt !== '' && <p className={classes.rankText}>{rank_txt}</p>}
                     <p className={classes.info}>{car_make_name}</p>
                     <Link href={slink}>
                         <h3>{title}</h3>
